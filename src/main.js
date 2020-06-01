48d9d2de316b30a0e688ca283d24bc9b0aa7eea6
superDragon <admin@jixiaokang.com>
2020-06-01 14:48:46 +0800

    feat:genxgin

--- src/main.js
+++ src/main.js
@@ -11,17 +11,17 @@ Vue.config.productionTip = false;
 Vue.prototype.utools = window.utools;
 Vue.prototype.$api = Api;
 // eslint-disable-next-line no-undef
-if (process.env.NODE_ENV === "production") {
-  window.utools.onPluginReady(() => {
-    console.log("插件装配完成，已准备好");
-    new Vue({
-      render: (h) => h(App),
-      store: store,
-    }).$mount("#app");
-  });
-} else {
+// if (process.env.NODE_ENV === "production") {
+//   window.utools.onPluginReady(() => {
+//     console.log("插件装配完成，已准备好");
+//     new Vue({
+//       render: (h) => h(App),
+//       store: store,
+//     }).$mount("#app");
+//   });
+// } else {
   new Vue({
     render: (h) => h(App),
     store: store,
   }).$mount("#app");
-}
+// }
